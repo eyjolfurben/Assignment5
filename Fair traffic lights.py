836ea5949fd4bcfb6ad5ea32cabff0fb8e1aed35
Harpa María <harpaf19@skel.ru.is>
2020-01-30 15:15:58 +0000

    Update Fair traffic lights.py

--- Fair traffic lights.py
+++ Fair traffic lights.py
@@ -3,8 +3,20 @@ south_int = int(input("Number of cars travelling south: "))
 east_int = int(input("Number of cars travelling east: "))
 west_int = int(input("Number of cars travelling west: "))
 
-sumof_north_int=0
-sumof_south_int=0
-sumof_east_int=0
-sumof_west_int=0
+sum_of_cars = north_int + south_int + east_int + west_int
 
+n/s = 0
+e/w = 0
+
+while sum_of_cars > 0:
+    if n/s > e/w:
+        print("Green light on N/S")
+    else:
+
+"""
+while ((north_int or south_int) > (east_int or west_int)):
+    print("Green light on N/S")
+
+
+print("Green light on E/W")
+print("No cars waiting, the traffic jam has been solved!")
