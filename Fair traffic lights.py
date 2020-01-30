81871c729b80e295a2561ced74e6320ac26cb21c
eyjolfurben <eyjolfurben@gmail.com>
2020-01-30 15:04:34 +0000

    Add files via upload

---
+++ Fair traffic lights.py
@@ -0,0 +1,10 @@
+north_int = int(input("Number of cars travelling north: "))
+south_int = int(input("Number of cars travelling south: "))
+east_int = int(input("Number of cars travelling east: "))
+west_int = int(input("Number of cars travelling west: "))
+
+sumof_north_int=0
+sumof_south_int=0
+
+
+while north_int
